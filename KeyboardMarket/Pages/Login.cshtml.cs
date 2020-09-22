099c1fead5f5ac0d652c47a58e89a0931a326126
Christian Höller <christianholler@Christians-MacBook-Pro.local>
2020-09-22 21:13:35 +0200

    migrated to .net core (once again) with basic microsoft login

---
+++ KeyboardMarket/Pages/Login.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KeyboardMarket.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostLoginMicrosoft()
+        {
+            await HttpContext.ChallengeAsync();
+        }
+    }
+}
---
+++ KeyboardMarket/Pages/Shared/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KeyboardMarket.Pages.Shared
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostLogout()
+        {
+            await HttpContext.SignOutAsync();
+            Response.Redirect("index");
+        }
+    }
+}
