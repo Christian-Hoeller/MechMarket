6d4580b9db49e1d75eac9a4e65beee9c12e46893
Christian Höller <christianholler@Christians-MacBook-Pro.local>
2020-09-25 10:53:29 +0200

    check if user is registered

--- KeyboardMarket/Pages/Login.cshtml.cs
+++ KeyboardMarket/Pages/Login.cshtml.cs
@@ -9,35 +9,64 @@ using KeyboardMarket.Classes;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace KeyboardMarket.Pages
 {
     public class LoginModel : PageModel
     {
+
+        public IEnumerable<Claim> Claims { get; set; }
+        public string EmailClaim { get; set; }
+
+        private string Email { get; set; }
+
         public void OnGet()
         {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                Email = User.FindFirstValue(ClaimTypes.Email);
+
+                if (UserIsRegistered() == false)
+                {
+                    RegisterUser();
+                }
+                else
+                {
+                    Response.Redirect("index");
+                }
+            }
+            else
+            {
+                //user is not authenticated
+            }
+        }
+
+        private void RegisterUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool UserIsRegistered()
+        {
+            DBContext dBContext = new DBContext();
+            DataTable dt = new DataTable();
+
+            using (SqlCommand cmd = new SqlCommand($"SELECT * FROM Users WHERE Email = {Email}"))
+            {
+                dt = dBContext.GetDataReader(cmd);
+            }
+
+            return dt.Rows.Count != 0;
         }
 
         public async Task OnPostLoginMicrosoft()
         {
             await HttpContext.ChallengeAsync();
 
-            //    DBContext dBContext = new DBContext();
-            //    DataTable dt = new DataTable();
-
-            //    using(SqlCommand cmd = new SqlCommand("SELECT * FROM [dbo].[Lands]"))
-            //    {
-            //        dt = dBContext.GetDataReader(cmd);
-            //    }
-
-            //    List<string> Lands = new List<string>();
-
-
-            //    foreach(DataRow row in dt.Rows)
-            //    {
-            //        Lands.Add(row["Land"].ToString());
-            //    }
-            //}
+           
         }
     }
 }
