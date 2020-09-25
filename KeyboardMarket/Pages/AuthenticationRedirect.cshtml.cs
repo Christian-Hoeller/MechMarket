9ac4c7ff9a7ec8487f08c9ec0fd92e7e0180ca7c
Christian Höller <christianholler@Christians-MacBook-Pro.local>
2020-09-25 20:12:10 +0200

    added redirect Authentication and Registration-check

---
+++ KeyboardMarket/Pages/AuthenticationRedirect.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using KeyboardMarket.Classes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KeyboardMarket.Pages
+{
+    public class AuthenticationRedirectModel : PageModel
+    {
+        private string Email { get; set; }
+
+        public void OnGet()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                Email = User.FindFirstValue(ClaimTypes.Email);
+
+                if (UserIsRegistered() == false)
+                {
+                    RedirectTo("register-user");
+                }
+                else
+                {
+                    RedirectTo("index");
+                }
+            }
+            else
+            {
+                 RedirectTo("index");
+            }
+        }
+
+        private void RedirectTo(string page)
+        {
+            Response.Redirect(page);
+        }
+
+        private bool UserIsRegistered()
+        {
+            DBContext dBContext = new DBContext();
+            DataTable dt = new DataTable();
+
+            using (SqlCommand cmd = new SqlCommand($"SELECT * FROM Users WHERE Email = '{Email}'"))
+            {
+                dt = dBContext.GetDataReader(cmd);
+            }
+
+            return dt.Rows.Count != 0;
+        }
+    }
+}
--- KeyboardMarket/Pages/Login.cshtml.cs
+++ KeyboardMarket/Pages/Login.cshtml.cs
@@ -16,57 +16,17 @@ namespace KeyboardMarket.Pages
 {
     public class LoginModel : PageModel
     {
-
-        public IEnumerable<Claim> Claims { get; set; }
-        public string EmailClaim { get; set; }
-
-        private string Email { get; set; }
-
         public void OnGet()
         {
-
             if (User.Identity.IsAuthenticated)
             {
-                Email = User.FindFirstValue(ClaimTypes.Email);
-
-                if (UserIsRegistered() == false)
-                {
-                    RegisterUser();
-                }
-                else
-                {
-                    Response.Redirect("index");
-                }
+                Response.Redirect("authenticationRedirect");
             }
-            else
-            {
-                //user is not authenticated
-            }
-        }
-
-        private void RegisterUser()
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool UserIsRegistered()
-        {
-            DBContext dBContext = new DBContext();
-            DataTable dt = new DataTable();
-
-            using (SqlCommand cmd = new SqlCommand($"SELECT * FROM Users WHERE Email = {Email}"))
-            {
-                dt = dBContext.GetDataReader(cmd);
-            }
-
-            return dt.Rows.Count != 0;
         }
 
         public async Task OnPostLoginMicrosoft()
         {
             await HttpContext.ChallengeAsync();
-
-           
         }
     }
 }
